33cfda6dbaaa1369a20b31dd00a6fe8563a7c32e
Carlo Gilmar <carlogilmar12@gmail.com>
2022-04-11 20:37:11 -0500

    3. Agregando más console.log

--- weekly_mission_1/main.js
+++ weekly_mission_1/main.js
@@ -1,1 +1,6 @@
 console.log("1) Hello world!")
+console.log("1) Hello world!")
+console.log("1) Hello world!")
+console.log("1) Hello world!")
+console.log("1) Hello world!")
+console.log("1) Hello wo
